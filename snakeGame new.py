7c3aae4a64d2542a1b07290893ab4e1818e22ae9
r.id <rahat.id03@gmail.com>
2024-01-12 03:31:55 +0000

    snake game

--- snakeGame new.py
+++ snakeGame new.py
@@ -25,8 +25,8 @@ class Snake:
 
 class Food:
     def __init__(self):
-        x = random.randint(0, (GAME_WIDTH // SPACE_SIZE) - 1) * SPACE_SIZE
-        y = random.randint(0, (GAME_HEIGHT // SPACE_SIZE) - 1) * SPACE_SIZE
+        x = random.randint(0, int(GAME_WIDTH // SPACE_SIZE) - 1) * SPACE_SIZE
+        y = random.randint(0, int(GAME_HEIGHT // SPACE_SIZE) - 1) * SPACE_SIZE
 
         self.coordinates = [x, y]
 
